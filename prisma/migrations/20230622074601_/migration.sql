36740fe6ca9c0fcc347e74a99f98a2430a4e9158
khalvai <khalvayozbek@gmail.com>
2023-06-22 11:34:34 +0330

    add slider schema

--- prisma/migrations/20230622074601_/migration.sql
+++ prisma/migrations/20230622074601_/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user'
 );
 
 -- CreateTable
@@ -20,6 +22,23 @@ CREATE TABLE "Post" (
     "authorId" INTEGER NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "FAQ" (
+    "id" SERIAL NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updateAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Slider" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "uploadedImagePath" TEXT NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
@@ -31,6 +50,12 @@ CREATE UNIQUE INDEX "Category_id_key" ON "Category"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Post_id_key" ON "Post"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FAQ_id_key" ON "FAQ"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Slider_id_key" ON "Slider"("id");
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
